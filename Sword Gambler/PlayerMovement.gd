af82193ea87190f14cf43cbf7c92402d0a4bb55e
JDiBiasi <53409876+JDiBiasi@users.noreply.github.com>
2022-09-20 20:29:53 -0400

    Fixes
    Restricted player movement to view port, fixed player sprite, added walking animation

--- Sword Gambler/PlayerMovement.gd
+++ Sword Gambler/PlayerMovement.gd
@@ -1,23 +1,39 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
 
+export (int) var speed = 400
 var velocity = Vector2()
+var screen_size
 onready var position2D = $Position2D
+
+func _ready():
+	screen_size = get_viewport().size
+
 func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
-		position2D.scale.x=-4
+		position2D.scale.x=-1
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
-		position2D.scale.x=4
+		position2D.scale.x=1
 	if Input.is_action_pressed("down"):
 		velocity.y += 1
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
+	
 	velocity = velocity.normalized() * speed
 
-func _physics_process(delta):
+func _process(_delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+	
+	if velocity.length() > 0:
+		$Position2D/AnimatedSprite.play("walk")
+	else:
+		$Position2D/AnimatedSprite.stop()
+	
+	
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
