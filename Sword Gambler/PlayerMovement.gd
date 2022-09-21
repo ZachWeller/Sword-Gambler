c62fd0b6e399fc31ab5f71eb3ce73f1c82c6e13a
JDiBiasi <53409876+JDiBiasi@users.noreply.github.com>
2022-09-21 02:35:09 -0400

    Sword Swing
    Sorta fixed the sword swing?

--- Sword Gambler/PlayerMovement.gd
+++ Sword Gambler/PlayerMovement.gd
@@ -5,7 +5,6 @@ export (int) var speed = 400
 var velocity = Vector2()
 var screen_size
 onready var position2D = $Position2D
-onready var swordSprite = $Position2D/rightSwordSprite
 func _ready():
 	screen_size = get_viewport().size
 
@@ -15,32 +14,29 @@ func get_input():
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
 		position2D.scale.x=-1
-		swordSprite.position.x=-8
 	if Input.is_action_pressed("left"):
 		velocity.x -= 1
 		position2D.scale.x=1
-		swordSprite.position.x = -8
 	if Input.is_action_pressed("down"):
 		velocity.y += 1
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
-	if Input.is_action_pressed("attack"):
-		swordSprite.play("right attack")
-
-		
+	
 	velocity = velocity.normalized() * speed
 
-
-	
 func _process(_delta):
 	get_input()
 	velocity = move_and_slide(velocity)
 	
 	if velocity.length() > 0:
-		$Position2D/AnimatedSprite.play("walk")
+		$Position2D/Player.play("walk")
 	else:
-		$Position2D/AnimatedSprite.stop()
+		$Position2D/Player.stop()
 	
+	if Input.is_action_pressed("attack"):
+		$Position2D/Sword.frame = 1
+	else	:
+		$Position2D/Sword.frame = 0
 	
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
