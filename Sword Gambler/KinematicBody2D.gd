d1311f91cd9dd9aff77164874a7c5e0c5548a567
ZachWeller <112107584+ZachWeller@users.noreply.github.com>
2022-10-01 01:50:03 -0400

    Walking Diagonal Fixed
    While walking up right, it displays right walking animation

--- Sword Gambler/KinematicBody2D.gd
+++ Sword Gambler/KinematicBody2D.gd
@@ -1,25 +1,83 @@
 extends KinematicBody2D
 
-export (int) var speed = 200
+export (int) var speed = 100
+var animation_direction = "Down"
+var animation_mode = "Idle"
+var animation
+var velocity = Vector2(0,0)
 
-var velocity = Vector2()
+func _physics_process(delta):
+	normal_walk()
+		
+func _process(delta):
+	AnimationLoop()
 
-func get_input():
-	velocity = Vector2()
-	if Input.is_action_pressed("right"):
-		velocity.x += 1
-		$AnimationPlayer.play("Walk_Right")
-	if Input.is_action_pressed("left"):
-		velocity.x -= 1
-		$AnimationPlayer.play("Walk_Left")
-	if Input.is_action_pressed("down"):
-		velocity.y += 1
-		$AnimationPlayer.play("Walk_Down")
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
-		$AnimationPlayer.play("Walk_Up")
-	velocity = velocity.normalized() * speed
+func normal_walk():
+	velocity.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	velocity.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+	var motion = velocity.normalized() * speed
+	move_and_slide(motion)
 	
-func _physics_process(_delta):
-	get_input()
-	velocity = move_and_slide(velocity)
+func AnimationLoop():
+	match velocity:
+		Vector2(-1,0):
+			animation_direction = "Left"
+		Vector2(1,0):
+			animation_direction = "Right"
+		Vector2(0,1):
+			animation_direction = "Down"
+		Vector2(0,-1):
+			animation_direction = "Up"
+		Vector2(-1,-1):
+			animation_direction = "Left"
+		Vector2(-1,1):
+			animation_direction = "Left"
+		Vector2(1,-1):
+			animation_direction = "Right"
+		Vector2(1,1):
+			animation_direction = "Right"
+		
+	if velocity != Vector2(0,0):
+		animation_mode = "Walk"
+	else:
+		animation_mode = "Idle"
+	animation = animation_mode + "_" + animation_direction
+	$AnimationPlayer.play(animation)
+	print(animation)
+	
+	
+#func sword_walk():
+#	velocity = Vector2()
+##	Walking Right Animtion
+#	if Input.is_action_pressed("right"):
+#		$AnimationPlayer.play("Sword_Right_Walk")
+#		velocity.x += 1
+#	elif Input.is_action_just_released("right"):
+#		$AnimationPlayer.stop()
+#		$AnimationPlayer.play("Right_Idle")
+##	Walking Left Animation
+#	if Input.is_action_pressed("left"):
+#		velocity.x -= 1
+#		$AnimationPlayer.play("Sword_Left_Walk ")
+#	elif Input.is_action_just_released("left"):
+#		$AnimationPlayer.stop()
+#		$AnimationPlayer.play("Left_Idle")
+##	Walking Down Animation
+#	if Input.is_action_pressed("down"):
+#		velocity.y += 1
+#		$AnimationPlayer.play("Sword_Front_Walk")
+#	elif Input.is_action_just_released("down"):
+#		$AnimationPlayer.stop()
+#		$AnimationPlayer.play("Front_Idle")
+##	Walking Up Animation
+#	if Input.is_action_pressed("up"):
+#		velocity.y -= 1
+#		$AnimationPlayer.play("Sword_Back_Walk")	
+#	elif Input.is_action_just_released("up"):
+#		$AnimationPlayer.stop()
+#		$AnimationPlayer.play("Back_Idle")
+#
+	velocity = velocity.normalized() * speed
+
+
+
---
+++ Sword Gambler/swordSprite.gd
@@ -0,0 +1,12 @@
+extends AnimatedSprite
+
+
+
+func _ready():
+	look_at(get_global_mouse_position())
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
